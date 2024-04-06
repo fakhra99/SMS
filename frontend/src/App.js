994d2e08a7b4f5d21a6dcc300761026071592cf9
Fakhra99 <fakhrahnajeeb61@gmail.com>
2024-04-06 22:22:31 +0500

    dashboard layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Layout/Header/Header";
+import Sidebar from "./Layout/Sidebar/Sidebar";
 
 function App() {
   return (
   <>
    <Header/>
+   <Sidebar/>
   </>
   );
 }
