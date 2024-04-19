48eb5dc0a065d9eb8fb71b7cc9066a83a7f3fe35
somi batool <somibatool44@gmail.com>
2024-04-19 12:07:34 +0500

    bugsfixed

--- frontend/src/Pages/SignIn/SignIn.jsx
+++ frontend/src/Pages/SignIn/SignIn.jsx
@@ -4,9 +4,21 @@ import React from 'react';
 
 const SignIn = () => {
   return (
-    <div className="min-h-screen bg-white flex items-center justify-center">
-      <form className="w-full max-w-sm bg-cyan-200 shadow-md rounded-md px-8 py-5 border border-gray-300">
+    <div className="min-h-screen bg-white  flex items-center justify-center">
+      <form className="w-full border-gray-500 max-w-sm bg-white shadow-md rounded-md px-8 py-5 border ">
         <h2 className="text-2xl font-medium text-center mb-6">Sign In</h2>
+        <div className="mb-6">
+          <label htmlFor="text" className="block text-sm font-medium text-gray-700 mb-2">
+            User Name
+          </label>
+          <input
+            type="text"
+            id="text"
+            className="shadow appearance-none border rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Enter your User Name"
+            required
+          />
+        </div>
         <div className="mb-6">
           <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-2">
             Email Address
@@ -31,12 +43,25 @@ const SignIn = () => {
             required
           />
         </div>
+
+        <div className="mb-6">
+          <label htmlFor="password" className="block text-sm font-medium text-gray-700 mb-2">
+          confirmPassword
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="shadow appearance-none border rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Confirm your password"
+            required
+          />
+        </div>
         <div className="flex items-center justify-between">
           <button
             type="submit"
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
           >
-            SignIn
+            Sign In
           </button>
           <a href="#" className="inline-block p-px align-baseline font-normal text-sm text-blue-500 hover:text-blue-700">
             ForgotPassword?
