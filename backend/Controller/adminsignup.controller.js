f0f236b904d20a246cf50796a30a1b0c199d09fc
Fakhra99 <fakhrahnajeeb61@gmail.com>
2024-05-03 00:01:27 +0500

    added signup and login-backend

---
+++ backend/Controller/adminsignup.controller.js
@@ -0,0 +1,47 @@
+import User from "../Model/adminsignup.model.js"
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+// register
+export const register = async (req, res) => {
+
+    try {
+        console.log("Called")
+        const {username, email, password} = req.body
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(401).json({ message: "User Already exists" });
+        }
+        const hashPassword = await bcrypt.hash(password, 10)
+
+        const newUser = new User({username, email, password: hashPassword });
+        await newUser.save()
+        res.status(201).json(newUser)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+//login
+export const login = async (req, res,next) => {
+    try{
+        const user = await User.findOne({email: req.body.email});
+        
+        if(!user) return next(console.log("user not found"));
+
+        const isCorrect = bcrypt.compareSync(req.body.password,user.password);
+        if(!isCorrect) return next(console.log(400,"Wrong password or email!"));
+
+        const token = jwt.sign({
+            id: user._id, 
+            role: user.role
+        },
+        process.env.PRIVATE_KEY, { expiresIn: "30d" })
+// destructuring to create new object info(contains all properties of the user._doc object except password. 
+// the user data retrieved from a database, and this step ensures that the password is not included in the response.
+        const {password, ...info} = user._doc;
+        res.status(200).json({message:"success", user:info, token})
+    }catch(err){
+        next(err)
+    }
+}
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,28 @@
 import express from "express";
 import mongoose from "mongoose";
-import 'dotenv/config';
+import dotenv from 'dotenv';
 const app = express();
+import AdminSignup from "./Route/adminsignup.route.js"
+import bodyParser from "body-parser";
+
+
+// Load environment variables from the .env file
+dotenv.config();
+
+// Use bodyParser middleware
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Define routes
+app.use("/api", AdminSignup);
 
 // Extract MongoDB credentials from environment variables
 const username = process.env.USER;
 const password = process.env.PASSWORD;
 
-// console.log(username)
-// console.log(password)
+console.log(username)
+console.log(password)
 
 // Use the environment variable PORT if defined, otherwise use 4041
 const PORT = process.env.PORT || 4041;
