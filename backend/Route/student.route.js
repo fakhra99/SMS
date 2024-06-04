6ad4eaa3e8e4536556ded9c03d2b64d718803af4
Fakhra99 <fakhrahnajeeb61@gmail.com>
2024-06-04 15:50:07 +0500

    resolved some clashes

--- backend/Route/student.route.js
+++ backend/Route/student.route.js
@@ -1,9 +1,6 @@
 import express, { Router } from 'express';
-<<<<<<< HEAD
-import { register, getStudentsData, delStudent, update, countStudents, getGenderDistribution } from '../Controller/student.controller.js';
-=======
-import { register, getStudentsData, delStudent, update, assignStudent } from '../Controller/student.controller.js';
->>>>>>> backendcreateclasses
+import { register, getStudentsData, delStudent, update, countStudents, getGenderDistribution , assignStudent} from '../Controller/student.controller.js';
+// import { register, getStudentsData, delStudent, update,  } from '../Controller/student.controller.js';
 import multer from 'multer';
 import path from 'path';
 
@@ -27,11 +24,8 @@ router.post("/addStudent", upload.single('Image'), register)
 router.get("/allStudents", getStudentsData)
 router.delete("/delStudent/:id", delStudent)
 router.put("/update/:id", update);
-<<<<<<< HEAD
 router.get("/countStudents", countStudents);
-=======
 router.post("/assignstudent", assignStudent);
->>>>>>> backendcreateclasses
 
 router.get('/students/gender-distribution', getGenderDistribution);
 export default router;
