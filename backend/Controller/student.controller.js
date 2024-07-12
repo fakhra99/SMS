d89732381feaa68337b46749da90fbc31b4d3991
Malaika Saba <malaikasaba3@gmail.com>
2024-07-12 11:48:33 +0500

    feevoucher changes

--- backend/Controller/student.controller.js
+++ backend/Controller/student.controller.js
@@ -157,7 +157,6 @@ export const getStudentById = async (req, res) => {
 
         res.status(200).json({ student });
     } catch (error) {
-        console.error(error);
         res.status(500).json({ message: error.message });
     }
 };
